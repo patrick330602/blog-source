3bd3b120349abd8e42c2cf7e9110b0702c2423a0
Jinming Wu, Patrick <me@patrickwu.space>
2022-03-26 11:13:25 +0800

    add error documents

--- source/error.php
+++ source/error.php
@@ -6,7 +6,7 @@
         body,
         html {
             font-family: Roboto, "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-            color: white;
+            color: black;
         }
 
         a {
@@ -26,67 +26,6 @@
         }
         a:visited {
             color: aliceblue;
-        }
-
-        #bg {
-            background-image: url("https://cdn.patrickwu.space/base/404-bg.gif");
-            background-repeat: no-repeat;
-            background-attachment: scroll;
-            background-clip: border-box;
-            background-origin: padding-box;
-            background-size: cover;
-            background-position: bottom;
-            position: fixed;
-            width: 100%;
-            height: 100%;
-            top: 0px;
-            left: 0px;
-            z-index: -1;
-        }
-
-        #cover {
-            background-color: rgba(0, 0, 0, 0.9);
-            opacity: 0.8;
-            position: fixed;
-            width: 100%;
-            height: 100%;
-            top: 0px;
-            left: 0px;
-            z-index: -1;
-        }
-
-        #content {
-            text-align: center;
-            position: center center;
-            align-content: center;
-        }
-
-        #fof {
-            text-align: center;
-            font-size: 80px;
-            color: grey;
-        }
-
-        #fog {
-            text-align: center;
-            font-size: 30px;
-            color: grey;
-        }
-
-        #parent {
-            text-align: center;
-            font-size: 12px;
-        }
-
-        #child {
-            text-align: center;
-            padding: 20px 80px;
-            display: none;
-            font-size: 10px;
-        }
-
-        #parent:hover #child {
-            display: block;
         }
     </style>
     <title>Error <?= http_response_code() ?></title>
