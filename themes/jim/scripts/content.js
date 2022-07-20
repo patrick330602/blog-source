80d8c30993fa0cde2c171ae72b95f37759c60bbf
Patrick Wu <me@patrickwu.space>
2022-07-20 11:25:29 +0800

    Force escape all url reserved characters

--- themes/jim/scripts/content.js
+++ themes/jim/scripts/content.js
@@ -164,5 +164,5 @@ hexo.extend.helper.register('toc_list', (content) => {
 });
 
 hexo.extend.helper.register('escape_string_for_url', (content) => {
-    return content.replace(/#/g, '%23');
+    return content.replace(/\!/g, '%21').replace(/\*/g, '%2A').replace(/\'/g, '%27').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/;/g, '%3B').replace(/:/g, '%3A').replace(/@/g, '%40').replace(/&/g, '%26').replace(/=/g, '%3D').replace(/\+/g, '%2B').replace(/\$/g, '%24').replace(/,/g, '%2C').replace(/\//g, '%2F').replace(/\?/g, '%3F').replace(/%/g, '%25').replace(/\#/g, '%23').replace(/\[/g, '%5B').replace(/\]/g, '%5D');
 });
